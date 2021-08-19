50eb8bb85f2bca2eaf9798c899d87db66ddfb6ad
danymmereles <mdmereles30@gmail.com>
2021-08-19 18:32:49 -0300

    Getters y Setters Usuario

--- Parcial codeado/src/main/java/Security/Usuario.java
+++ Parcial codeado/src/main/java/Security/Usuario.java
@@ -5,8 +5,43 @@ import Security.Roles.Rol;
 
 public class Usuario {
 
+    //ATRIBUTOS
     private String nombreUsuario;
     private String password;
     private Rol rol;
+
+    //CONSTRUCTOR
+    public Usuario(String usuario, String contrasenia){
+        if(new ValidadoresEnAccion().esValida(contrasenia)){
+            this.nombreUsuario = usuario;
+            this.password = contrasenia;
+        }
+    }
+
+    //GETTERS
+    public String getNombreUsuario(){
+        return nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    //SETTERS
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
 
 }
