42832fc52b913bc1d42451896d8c2776de0880be
danymmereles <mdmereles30@gmail.com>
2021-08-19 18:11:01 -0300

    Posible alternativa, Mati decide

--- Parcial codeado/src/main/java/Security/Usuario.java
+++ Parcial codeado/src/main/java/Security/Usuario.java
@@ -5,8 +5,19 @@ import Security.Roles.Rol;
 
 public class Usuario {
 
-    String nombreUsuario;
-    String password;
-    Rol rol;
+    private String nombreUsuario;
+    private String password;
+    private Rol rol;
+
+    /*
+    public boolean PasswordValida (String supuestaClave){
+
+        boolean nula = supuestaClave.isEmpty();
+        boolean esCorta = supuestaClave.length() < 10;
+
+        return (nula && esCorta);
+
+    }
+     */
 
 }
