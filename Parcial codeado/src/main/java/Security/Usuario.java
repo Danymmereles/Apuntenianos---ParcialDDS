ddcd4db7978a259ac4e6d64598a1994ab332e203
danymmereles <mdmereles30@gmail.com>
2021-08-19 18:18:52 -0300

    Mati decidio y el pueblo escucho

--- Parcial codeado/src/main/java/Security/Usuario.java
+++ Parcial codeado/src/main/java/Security/Usuario.java
@@ -9,15 +9,4 @@ public class Usuario {
     private String password;
     private Rol rol;
 
-    /*
-    public boolean PasswordValida (String supuestaClave){
-
-        boolean nula = supuestaClave.isEmpty();
-        boolean esCorta = supuestaClave.length() < 10;
-
-        return (nula && esCorta);
-
-    }
-     */
-
 }
