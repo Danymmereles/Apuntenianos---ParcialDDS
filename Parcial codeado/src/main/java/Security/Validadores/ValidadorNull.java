480fa7a92bce91142fd99b1d6f9649f52e04690a
danymmereles <mdmereles30@gmail.com>
2021-08-20 18:26:51 -0300

    Validadores mas resumidos

--- Parcial codeado/src/main/java/Security/Validadores/ValidadorNull.java
+++ Parcial codeado/src/main/java/Security/Validadores/ValidadorNull.java
@@ -4,11 +4,6 @@ package Security.Validadores;
 public class ValidadorNull implements Validador{
     @Override
     public boolean esValido(String password) {
-        if (password.isEmpty()) {
-            return false;
-        }
-        else {
-            return true;
-        }
+        return password.isEmpty();
     }
 }
--- Parcial codeado/src/main/java/Security/Validadores/ValidadorTamanio.java
+++ Parcial codeado/src/main/java/Security/Validadores/ValidadorTamanio.java
@@ -1,15 +1,13 @@
 package Security.Validadores;
 
 public class ValidadorTamanio implements Validador{
+
     @java.lang.Override
     public boolean esValido(String password) {
-        int tamanioPassword = password.length();
 
-        if (tamanioPassword > 10) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return password.length() > 10;
+
     }
+
+
 }
