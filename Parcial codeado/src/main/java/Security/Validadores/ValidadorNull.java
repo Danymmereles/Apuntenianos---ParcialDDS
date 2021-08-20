19ee86de5dca3e7aafadc96ef379189d5e76b8a1
danymmereles <mdmereles30@gmail.com>
2021-08-20 18:28:13 -0300

    Casi hago lio

--- Parcial codeado/src/main/java/Security/Validadores/ValidadorNull.java
+++ Parcial codeado/src/main/java/Security/Validadores/ValidadorNull.java
@@ -4,6 +4,6 @@ package Security.Validadores;
 public class ValidadorNull implements Validador{
     @Override
     public boolean esValido(String password) {
-        return password.isEmpty();
+        return !(password.isEmpty());
     }
 }
