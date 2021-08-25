79bfab49f1b1e6d5893b5f35002f412d38fe89df
danymmereles <mdmereles30@gmail.com>
2021-08-24 23:27:21 -0300

    Error de tipeo parte 4

--- Parcial codeado/src/main/java/Database/DBUsuario.java
+++ Parcial codeado/src/main/java/Database/DBUsuario.java
@@ -12,20 +12,34 @@ public class DBUsuario {
 
 
     public static void agregarUsuario (Usuario nuevoUsuario){
-
-    }
-
-    public static void eliminarUsuario (Usuario usuarioAEliminar){
-
-    }
-
-    public static void cambiarDatosSinMail (Usuario usuarioAModificar){
         String consulta;
         ResultSet resultado = null;
         Connection conexion;
         Statement fraseAEjecutar;
 
-        consulta = "UPDATE usuario SET tipoUsuario = '" + usuarioAModificar.rolUsuario() + "' WHERE mailUsuario = '"+ usuarioAModificar.getMail() +"';";
+        consulta = "INSERT INTO Usuario VALUES (NULL, '"+ nuevoUsuario.getMail() +"';";
+
+        try {
+            conexion = ConexionDB.conectar();
+
+            // Ejecución
+            fraseAEjecutar = conexion.createStatement();
+            resultado = fraseAEjecutar.executeQuery(consulta);
+            resultado.next();
+
+        } catch (SQLException error) {
+            // handle any errors
+            error.printStackTrace();
+        }
+    }
+
+    public static void eliminarUsuario (Usuario usuarioAEliminar){
+        String consulta;
+        ResultSet resultado = null;
+        Connection conexion;
+        Statement fraseAEjecutar;
+
+        consulta = "DELETE FROM Usuario WHERE mailUsuario = '"+ usuarioAEliminar.getMail() +"';";
 
         try {
             conexion = ConexionDB.conectar();
@@ -40,6 +54,29 @@ public class DBUsuario {
             error.printStackTrace();
         }
 
+
+    }
+
+    public static void actualizarUsuario (Usuario usuarioAModificar){
+        String consulta;
+        ResultSet resultado = null;
+        Connection conexion;
+        Statement fraseAEjecutar;
+
+        consulta = "UPDATE Usuario SET tipoUsuario = '" + usuarioAModificar.rolUsuario() + "' estado = '"+ usuarioAModificar.getPersona().getEstadoUsuario() +"' WHERE mailUsuario = '"+ usuarioAModificar.getMail() +"';";
+
+        try {
+            conexion = ConexionDB.conectar();
+
+            // Ejecución
+            fraseAEjecutar = conexion.createStatement();
+            resultado = fraseAEjecutar.executeQuery(consulta);
+            resultado.next();
+
+        } catch (SQLException error) {
+            // handle any errors
+            error.printStackTrace();
+        }
 
     }
 
